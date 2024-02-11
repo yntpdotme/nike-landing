6e002876969aab730e92c0ee42b17dedd281d004
Akash Kadlag <akashkadlag14@gmail.com>
2024-02-11 15:29:21 +0530

    chore: initialize project with basic structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <h1 className="text-3xl text-white text-center">
+      Lets Build Nike Website..!
+    </h1>
+  );
+};
+
+export default App;
---
+++ src/assets/icons/index.js
@@ -0,0 +1,27 @@
+import arrowRight from "./arrow-right.svg";
+import chevronLeft from "./chevron-left.svg";
+import cheveronRight from "./chevron-right.svg";
+import copyrightSign from "./copyright-sign.svg";
+import linkedin from "./linkedin.svg";
+import hamburger from "./hamburger.svg";
+import github from "./github.svg";
+import support from "./support.svg";
+import shieldTick from "./shield-tick.svg";
+import star from "./star.svg";
+import truckFast from "./truck-fast.svg";
+import twitter from "./twitter.svg";
+
+export {
+  arrowRight,
+  cheveronRight,
+  chevronLeft,
+  copyrightSign,
+  linkedin,
+  hamburger,
+  github,
+  support,
+  shieldTick,
+  star,
+  truckFast,
+  twitter,
+};
